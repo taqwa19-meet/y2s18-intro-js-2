221a844e2dd3835cb28ce9dead34d85ec1ce5ca6
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 16:55:49 +0300

    hello2

--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,4 +1,5 @@
 // Put all your JavaScript in this file!
-function alertMe(){
-	prompt("choose a color");
+function alertMe(object){
+	var aa = prompt("choose a color");
+	object.style.backgroundColor = aa;
 }
