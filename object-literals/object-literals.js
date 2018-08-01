392fa663674aa0f8eb874ab60df00d9793008d01
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 12:15:31 +0300

    hehe

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,9 @@
-// Create your 'me' object literal here!
+var me = {
+	firstname: "taqwa",
+	lastname: "zoubi",
+	favoriteFood:"sushi",
+	age:"16",
+	picture:"https://yt3.ggpht.com/a-/ACSszfFZdcVsQiee1jMSWsfa8h6hHWJnBKye1diktQ=s900-mo-c-c0xffffffff-rj-k-no"
+}
+me=JSON.stringify(me);
+document.write(me);
--- separation/code.js
+++ separation/code.js
@@ -1,1 +1,1 @@
-// Add your alert() here!
+alert("taqwa")
