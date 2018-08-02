5473b95aaa589705979af7420a2ceca16d4350d8
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 17:25:05 +0300

    events

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -1,13 +1,26 @@
 // YOUR TASK: Add more pictures!
-var pictures = ['./imgs/dog.jpg'];
 var currentIndex = 0;
-
+var pictures = ["./imgs/dog.jpg", "./imgs/express.jpeg", "./imgs/harry.jpeg", "./imgs/potter.jpeg", "./imgs/roof.jpeg"];
 function showNextPicture() {
   currentIndex++; // increment current picture
   // if currentIndex is too large, start from the beginning again
+
+
+  document.getElementsByTagName("img")[0].addEventListener("click", function (event) {
+     //event.target.innerHTML="ive been clicked";
+     alert("click the button");
+  
+});
+
+
   if (currentIndex >= pictures.length) {
     currentIndex = 0;
   }
 
+
+
   // YOUR TASK: Finish this function!
+  //i didnt finish the function still(part1.5,number5)
 }
+
+showNextPicture()
